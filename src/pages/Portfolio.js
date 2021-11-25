702119941c463dcd91316bf43b0f87c25158a5be
Chukwuemeka Nkwo <45833359+EmekaNkwo@users.noreply.github.com>
2021-11-25 01:25:50 +0100

    Add files via upload

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PortfolioSection from "../PortfolioSection";
+
+export default function Portfolio() {
+  return (
+    <div>
+      <PortfolioSection />
+    </div>
+  );
+}
---
+++ src/pages/Services.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ServicesSection from "../ServicesSection";
+
+export default function Services() {
+  return (
+    <div>
+      <ServicesSection />
+    </div>
+  );
+}
